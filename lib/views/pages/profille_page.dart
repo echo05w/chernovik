525dccd0f225e407ad0e4ffddd6845c74c0a2fb8
echo05w <soliyevazuhra03@gmail.com>
2025-11-17 18:45:44 +0500

    added Swticher and Checker

--- lib/views/pages/profille_page.dart
+++ lib/views/pages/profille_page.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
 
-class ProfillePage extends StatelessWidget {
+class ProfillePage extends StatefulWidget {
   const ProfillePage({super.key});
 
   @override
+  State<ProfillePage> createState() => _ProfillePageState();
+}
+
+class _ProfillePageState extends State<ProfillePage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Profile Page",
-        style: TextStyle(fontSize: 24),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: 'Username',
+              border: OutlineInputBorder(),
+            ),
+            onEditingComplete: () => setState(() {
+              FocusScope.of(context).unfocus();
+            }),
+          ),
+          Text(controller.text),
+          Checkbox(
+            tristate: true,
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          CheckboxListTile(
+            tristate: true,
+            title: const Text('Accept Terms and Conditions'),
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          Switch(
+            value: isSwitched,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+          SwitchListTile(value: isSwitched, onChanged:  (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            }, title: const Text('Enable Notifications'),),
+        ],
       ),
     );
   }
