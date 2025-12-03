1f0b564c77e33965b5a6f57209c165a3ece694c4
Hasan Soliyev <168918473+echo05w@users.noreply.github.com>
2025-12-03 05:39:43 +0500

    Delete testing directory

--- testing/test/login_test.dart
+++
@@ -1,8 +0,0 @@
-testWidgets('Login validation works', (WidgetTester tester) async {
-  await tester.pumpWidget(MaterialApp(home: LoginPage()));
-
-  await tester.tap(find.byType(ElevatedButton));
-  await tester.pump();
-
-  expect(find.text('Required'), findsWidgets);
-});
